b7c97d0501e584f7ba067845682f5ed25a101bb0
Jared Wilber <j.wilber92@gmail.com>
2019-08-06 23:42:28 -0700

    added SCRAPE_BASE_URL

--- skatevideosite/make_skaters_and_videos_df.py
+++ skatevideosite/make_skaters_and_videos_df.py
@@ -18,18 +18,19 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 BASE_URL = "http://www.skatevideosite.com/index.php?page=skatevideos&sort=rating&p={}"
-DICT_KEYS	 = ['company', 'filmmaker', 'year', 'country']
+SITE_BASE_URL = 'http://www.skatevideosite.com{video}'
+DICT_KEYS    = ['company', 'filmmaker', 'year', 'country']
 
 
 def load_soup(base_url):
-	# load a given url with beautifulsoup
+    # load a given url with beautifulsoup
     page = requests.get(base_url)
     soup = BeautifulSoup(page.content, 'html.parser')
     return soup
 
 
 def get_video_urls(soup):
-	# get urls to all skate videos on page
+    # get urls to all skate videos on page
     skate_videos = soup.find_all('table', {"id": "skatevideos"})
     trs = skate_videos[0].find_all('td')
     tr_links = [tr.find_all('a') for tr in trs]
@@ -49,13 +50,13 @@ def get_video_urls(soup):
 
 
 def get_skaters(soup):
-	# get names of all skaters listed on page
+    # get names of all skaters listed on page
     skater_links = soup.find_all('div', {"id": "skaterlist"})[0].find_all('a')
     return [a.text for a in skater_links]
 
 
 def get_video_info(soup):
-	# get basic info for current page's skate video
+    # get basic info for current page's skate video
     video_dict = {k:[] for k in DICT_KEYS}
     for a in soup.find_all('table', class_="videoinfo")[0].find_all('a'):
         if "colspan" in a.attrs:
@@ -75,12 +76,12 @@ def get_video_info(soup):
 
 
 def get_title(soup):
-	# get title of current page's skate video
+    # get title of current page's skate video
     return soup.find_all('div', class_='twelve columns')[0].find('h1').text
 
 
 def make_video_df(soup):
-	# create dataframe for current page's skate video
+    # create dataframe for current page's skate video
     lst_col = 'skater'
     skaters = get_skaters(soup)
     df = get_video_info(soup)
@@ -95,7 +96,7 @@ def make_video_df(soup):
 
 
 def make_videos_info(video_urls):
-	# create list of dataframes for all skate-videos on current page
+    # create list of dataframes for all skate-videos on current page
     videos_info = []
     for vid in video_urls:
         full_vid_url = SITE_BASE_URL.format(video=vid)
@@ -109,7 +110,7 @@ def make_videos_info(video_urls):
 
 
 def scrape_page(url):
-	# scrape current page
+    # scrape current page
     soup = load_soup(url)
     video_urls = get_video_urls(soup)
     videos_info = make_videos_info(video_urls)
@@ -118,7 +119,7 @@ def scrape_page(url):
 
 
 def scrape_all_videos(page_start, page_end):
-	# scrape pages `page_start` to `page_end` for info
+    # scrape pages `page_start` to `page_end` for info
     all_video_dfs = []
     for i in range(page_start, page_end):
         print('Scraping page :', i)
@@ -138,15 +139,15 @@ def resolve_name(name):
 
 if __name__ == '__main__':
 
-	all_videos_df = scrape_all_videos(1, 19)
+    all_videos_df = scrape_all_videos(1, 19)
 
-	all_videos_df['company'] = all_videos_df.company.apply(lambda x: resolve_name(x))
-	all_videos_df['year'] = all_videos_df.year.apply(lambda x: resolve_name(x))
-	all_videos_df['title'] = all_videos_df.title.apply(lambda x: resolve_name(x))
-	all_videos_df['filmmaker'] = all_videos_df.filmmaker.apply(lambda x: resolve_name(x))
-	all_videos_df['country'] = all_videos_df.country.apply(lambda x: resolve_name(x))
+    all_videos_df['company'] = all_videos_df.company.apply(lambda x: resolve_name(x))
+    all_videos_df['year'] = all_videos_df.year.apply(lambda x: resolve_name(x))
+    all_videos_df['title'] = all_videos_df.title.apply(lambda x: resolve_name(x))
+    all_videos_df['filmmaker'] = all_videos_df.filmmaker.apply(lambda x: resolve_name(x))
+    all_videos_df['country'] = all_videos_df.country.apply(lambda x: resolve_name(x))
 
-	# sort videos descending
-	all_videos_df.sort_values(by = ['company', 'year', 'title'], ascending=False, inplace=True)
+    # sort videos descending
+    all_videos_df.sort_values(by = ['company', 'year', 'title'], ascending=False, inplace=True)
 
-	all_video_dfs.to_csv('skaters_and_videos.csv', index=False)
+    all_video_dfs.to_csv('skaters_and_videos.csv', index=False)
